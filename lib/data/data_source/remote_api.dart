5723dd4b250d54bf1eee7edbef5d837ef523c6a4
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2022-11-17 10:03:31 +0700

    Tutorial 8: Web Service

---
+++ lib/data/data_source/remote_api.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:pbp_flutter_tutorial/data/model/to_do.dart';
+
+class RemoteApi{
+  static Future<List<ToDo>> fetchToDo() async {
+    var url = Uri.parse(
+        'https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10');
+    var response = await http.get(
+      url,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    );
+
+    // melakukan decode response menjadi bentuk json
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    // melakukan konversi data json menjadi object ToDo
+    List<ToDo> listToDo = [];
+    for (var d in data) {
+      if (d != null) {
+        listToDo.add(ToDo.fromJson(d));
+      }
+    }
+
+    return listToDo;
+  }
+}
